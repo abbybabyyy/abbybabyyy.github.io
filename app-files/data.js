cded58a0dc549aeb618b8a0462ec27c1eb9c8fcd
abbybabyyy <52609095+abbybabyyy@users.noreply.github.com>
2019-07-06 21:51:04 -0400

    First Website Upload

---
+++ app-files/data.js
@@ -0,0 +1,181 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen-2",
+      "name": "Kitchen 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -0.48616432804438325,
+        "pitch": 0.012219123600083037,
+        "fov": 1.218857579108061
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9315787426192088,
+          "pitch": 0.039223762559251085,
+          "rotation": 0,
+          "target": "2-living-room-3"
+        },
+        {
+          "yaw": 0.01188304857426914,
+          "pitch": 0.05748879024507758,
+          "rotation": 0,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": -1.3201062772022993,
+          "pitch": 0.221822348081524,
+          "rotation": 0,
+          "target": "3-living-room-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.86681147908825,
+          "pitch": 0.1936863581652215,
+          "rotation": 0,
+          "target": "3-living-room-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-living-room-3",
+      "name": "Living Room 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -1.8997644464132186,
+        "pitch": 0.10684004686433113,
+        "fov": 1.32956502764503
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.238253060598538,
+          "pitch": 0.14551127654820206,
+          "rotation": 0,
+          "target": "0-kitchen-2"
+        },
+        {
+          "yaw": -1.6776741402216935,
+          "pitch": 0.23804281256585647,
+          "rotation": 0,
+          "target": "3-living-room-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-living-room-4",
+      "name": "Living Room 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.020991473282968798,
+          "pitch": 0.33652536465690197,
+          "rotation": 6.283185307179586,
+          "target": "2-living-room-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
